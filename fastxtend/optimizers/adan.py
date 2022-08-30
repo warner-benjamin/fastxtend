892bf8bd2841e1e8748486209995f7609598be4b
Benjamin Warner <me@benjaminwarner.dev>
2022-08-30 12:35:15 -0500

    Simplify Adan implementation to use fewer callbacks

--- fastxtend/optimizers/adan.py
+++ fastxtend/optimizers/adan.py
@@ -14,66 +14,65 @@ from fastai.optimizer import Optimizer, step_stat
 from ..imports import *
 
 # Internal Cell
-def avg_grad(p, beta1, grad_avg=None, **kwargs):
-    "Tracks average gradients (m) of `p` in `state` with `beta1`."
-    if grad_avg is None:
-        grad_avg = torch.zeros_like(p, memory_format=torch.preserve_format)
-    grad_avg.mul_(beta1).add_(p.grad.data, alpha=1-beta1)
-    return {'grad_avg': grad_avg}
-
-avg_grad.defaults = dict(beta1=0.98)
+def debias(beta, step):
+    "Simple debias calculation"
+    return 1-beta**step
 
 # Internal Cell
-def avg_diff_grad(p, beta2, prior_grad=None, diff_avg=None, **kwargs):
-    "Tracks the average difference of current and prior gradients (v) of `p` in `state` with `beta2`."
-    if diff_avg is None:
-        diff_avg   = torch.zeros_like(p, memory_format=torch.preserve_format)
-        prior_grad = torch.zeros_like(p, memory_format=torch.preserve_format)
-
-    diff_avg.mul_(beta2).add_(p.grad.data-prior_grad, alpha=1-beta2)
-    return {'diff_avg': diff_avg}
-
-avg_diff_grad.defaults = dict(beta2=0.92)
-
-# Internal Cell
-def avg_nesterov_est(p, beta2, beta3, prior_grad=None, nesterov_est=None, **kwargs):
-    "Tracks the Nesterov momentum estimate of gradients (n) of `p` in `state` with `beta2` & `beta3`."
-    if nesterov_est is None:
+def adan_setup(p, step=0, grad_avg=None, diff_avg=None, nesterov_est=None, prior_grad=None, **kwargs):
+    "Handles Adan setup and keeps track of steps"
+    if step == 0:
+        grad_avg     = torch.zeros_like(p, memory_format=torch.preserve_format)
+        diff_avg     = torch.zeros_like(p, memory_format=torch.preserve_format)
         nesterov_est = torch.zeros_like(p, memory_format=torch.preserve_format)
         prior_grad   = torch.zeros_like(p, memory_format=torch.preserve_format)
+        step += 1
+        return {'grad_avg':grad_avg, 'diff_avg':diff_avg, 'nesterov_est':nesterov_est, 'prior_grad':prior_grad, 'step':step}
+    else:
+        step += 1
+        return {'step':step}
 
+# Internal Cell
+def adan_avgs(p, beta1, beta2, beta3, grad_avg, diff_avg, nesterov_est, prior_grad, **kwargs):
+    "Updates Adan moving averages"
+    # update m_k
+    grad_avg.mul_(beta1).add_(p.grad.data, alpha=1-beta1)
+
+    # update v_k
+    diff_avg.mul_(beta2).add_(p.grad.data-prior_grad, alpha=1-beta2)
+
+    # update n_k
     nesterov_est.mul_(beta3).add_(torch.square(torch.add(p.grad.data, torch.sub(p.grad.data, prior_grad), alpha=beta2)), alpha=1-beta3)
-    return {'nesterov_est': nesterov_est}
 
-avg_nesterov_est.defaults = dict(beta2=0.92, beta3=0.99)
+    # set current grad as next step's prior_grad
+    prior_grad = p.grad.data.clone()
+    return {'grad_avg':grad_avg, 'diff_avg':diff_avg, 'nesterov_est':nesterov_est, 'prior_grad':prior_grad}
 
-# Internal Cell
-def prior_grad(p, **kwargs):
-    "Register the current gradient of `p` for use in the next step"
-    return {'prior_grad' : p.grad.data.clone()}
-
-# Internal Cell
-def debias(beta, step):
-    return 1-beta**step
+adan_avgs.defaults = dict(beta1=0.98, beta2=0.92, beta3=0.99)
 
 # Internal Cell
 def adan_step(p, lr, eps, wd, beta1, beta2, beta3, step, grad_avg, diff_avg, nesterov_est, **kwargs):
     "Performs the Adan step with `lr` on `p`"
-    db1 = debias(beta1, step)
-    db2 = debias(beta2, step)
-    db3 = debias(beta3, step)
+    # calculate debias terms
+    db1, db2, db3 = debias(beta1, step), debias(beta2, step), debias(beta3, step)
+
+    # calculate applied λ
     wd = (1+lr*wd) if wd!=0 else 1
+
+    # calculate η_k
     lr = lr/torch.sqrt(nesterov_est/db3+eps)
+
+    # perform Adan step and apply to parameter `p`
     p.data.sub_(torch.add(grad_avg/db1, diff_avg/db2, alpha=beta2).mul_(lr)).div_(wd)
     return p
 
 # Cell
-def Adan(params, lr, beta1=0.98, beta2=0.92, beta3=0.99, eps=1e-8, wd=0.02):
+def Adan(params, lr, beta1=0.98, beta2=0.92, beta3=0.99, eps=1e-8, wd=0.02, less_steps=True):
     "A `Optimizer` for Adan with `lr`, `beta`s, `eps` and `params`"
-    cbs = [avg_grad, avg_diff_grad, avg_nesterov_est, prior_grad, step_stat, adan_step]
+    cbs = [adan_setup, adan_avgs, adan_step]
     return Optimizer(params, cbs, lr=lr, beta1=beta1, beta2=beta2, beta3=beta3, eps=eps, wd=wd)
 
 # Cell
 def AdanLargeBatchLR(bs):
-    "Square root rule for scaling Adan learning rate for large-batch training"
+    "Square root rule for scaling `Adan` learning rate for large-batch training"
     return math.sqrt(bs/256)*6.25e-3
