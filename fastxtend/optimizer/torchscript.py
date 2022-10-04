316dbfe0b5039bd15f972647da9232ebf0071658
Benjamin Warner <me@benjaminwarner.dev>
2022-10-04 15:37:20 -0500

    JitLookahead, jit ranger, and first docs pass

--- fastxtend/optimizer/torchscript.py
+++ fastxtend/optimizer/torchscript.py
@@ -15,7 +15,7 @@ from fastai.optimizer import (Optimizer, _update, weight_decay, l2_reg, average_
 from ..imports import *
 
 # %% auto 0
-__all__ = ['JitOptimizer', 'SGD', 'RMSProp', 'Adam', 'RAdam', 'QHAdam', 'Larc', 'Lamb', 'ranger']
+__all__ = ['JitOptimizer', 'SGD', 'RMSProp', 'Adam', 'RAdam', 'QHAdam', 'Larc', 'Lamb', 'JitLookahead', 'ranger']
 
 # %% ../../nbs/optimizer.torchscript.ipynb 8
 def _update(
@@ -26,6 +26,7 @@ def _update(
 
 # %% ../../nbs/optimizer.torchscript.ipynb 9
 class JitOptimizer(Optimizer):
+    "An `Optimizer` with a modified step for TorchScript optimizers"
     @torch.no_grad()
     def step(self, closure=None):
         if closure is not None: raise NotImplementedError("fastai optimizers currently do not support closure")
@@ -34,7 +35,7 @@ class JitOptimizer(Optimizer):
                 if hasattr(p, 'grad') and p.grad is not None:
                     _update(self.state[p], self.cbs[0](p, p.grad, **{**self.state[p], **hyper}))
 
-# %% ../../nbs/optimizer.torchscript.ipynb 11
+# %% ../../nbs/optimizer.torchscript.ipynb 12
 @torch.jit.script
 def sgd_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, decouple_wd:bool, grad_avg:Optional[Tensor]=None, 
                  do_wd:bool=True, dampening:bool=False, force_train:Optional[bool]=None):
@@ -66,9 +67,9 @@ def sgd_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, decouple_
         p.set_(dp)
         return None
 
-# %% ../../nbs/optimizer.torchscript.ipynb 12
+# %% ../../nbs/optimizer.torchscript.ipynb 13
 def SGD(params, lr, mom=0., wd=0., decouple_wd=True, jit=False):
-    "A `Optimizer` for SGD with `lr` and `mom` and `params`"
+    "A `Optimizer` or `JitOptimizer` for SGD with `lr` and `mom` and `params`"
     if jit:
         cb = partial(sgd_jit_step, decouple_wd=decouple_wd)
         return JitOptimizer(params, cb, lr=lr, mom=mom, wd=wd)
@@ -78,7 +79,7 @@ def SGD(params, lr, mom=0., wd=0., decouple_wd=True, jit=False):
         cbs.append(sgd_step if mom==0 else momentum_step)
         return Optimizer(params, cbs, lr=lr, mom=mom, wd=wd)
 
-# %% ../../nbs/optimizer.torchscript.ipynb 15
+# %% ../../nbs/optimizer.torchscript.ipynb 17
 @torch.jit.script
 def rmsprop_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom:float, eps:float, decouple_wd:bool, 
                      grad_avg:Optional[Tensor]=None, sqr_avg:Optional[Tensor]=None, do_wd:bool=True, force_train:Optional[bool]=None):
@@ -124,9 +125,9 @@ def rmsprop_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_m
         p.set_(dp)
         return {'sqr_avg': sqr_avg}
 
-# %% ../../nbs/optimizer.torchscript.ipynb 16
+# %% ../../nbs/optimizer.torchscript.ipynb 18
 def RMSProp(params, lr, sqr_mom=0.99, mom=0., eps=1e-8, wd=0., decouple_wd=True, jit=False):
-    "A `Optimizer` for RMSProp with `lr`, `sqr_mom`, `mom` and `params`"
+    "A `Optimizer` or `JitOptimizer` for RMSProp with `lr`, `sqr_mom`, `mom` and `params`"
     if jit:
         cb = partial(rmsprop_jit_step, decouple_wd=decouple_wd)
         return JitOptimizer(params, cb, lr=lr, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd)
@@ -136,12 +137,12 @@ def RMSProp(params, lr, sqr_mom=0.99, mom=0., eps=1e-8, wd=0., decouple_wd=True,
         cbs.append(rms_prop_step)
         return Optimizer(params, cbs, lr=lr, mom=mom, sqr_mom=sqr_mom, wd=wd, eps=eps)
 
-# %% ../../nbs/optimizer.torchscript.ipynb 20
+# %% ../../nbs/optimizer.torchscript.ipynb 23
 def debias(beta:float, step:int):
     "Simple debias calculation"
     return 1-beta**step
 
-# %% ../../nbs/optimizer.torchscript.ipynb 21
+# %% ../../nbs/optimizer.torchscript.ipynb 24
 @torch.jit.script
 def adam_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom:float, eps:float, 
                   decouple_wd:bool, grad_avg:Optional[Tensor]=None, sqr_avg:Optional[Tensor]=None, 
@@ -177,9 +178,9 @@ def adam_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom:
 
     return torch.jit.annotate(Dict[str, Union[Tensor, int]], {'grad_avg': grad_avg, 'sqr_avg': sqr_avg, 'step': step})
 
-# %% ../../nbs/optimizer.torchscript.ipynb 22
+# %% ../../nbs/optimizer.torchscript.ipynb 25
 def Adam(params, lr, mom=0.9, sqr_mom=0.99, eps=1e-5, wd=0.01, decouple_wd=True, jit=False):
-    "A `Optimizer` for Adam with `lr`, `mom`, `sqr_mom`, `eps` and `params`"
+    "A `Optimizer` or `JitOptimizer` for Adam with `lr`, `mom`, `sqr_mom`, `eps` and `params`"
     if jit:
         cb = partial(adam_jit_step, decouple_wd=decouple_wd)
         return JitOptimizer(params, cb, lr=lr, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd)
@@ -188,11 +189,11 @@ def Adam(params, lr, mom=0.9, sqr_mom=0.99, eps=1e-5, wd=0.01, decouple_wd=True,
         cbs += [partial(average_grad, dampening=True), average_sqr_grad, step_stat, adam_step]
         return Optimizer(params, cbs, lr=lr, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd)
 
-# %% ../../nbs/optimizer.torchscript.ipynb 25
+# %% ../../nbs/optimizer.torchscript.ipynb 29
 @torch.jit.script
 def radam_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom:float, eps:float, beta:float,
-                  decouple_wd:bool, grad_avg:Optional[Tensor]=None, sqr_avg:Optional[Tensor]=None, 
-                  do_wd:bool=True, step:int=0, force_train:Optional[bool]=None):
+                   decouple_wd:bool, grad_avg:Optional[Tensor]=None, sqr_avg:Optional[Tensor]=None,
+                   do_wd:bool=True, step:int=0, force_train:Optional[bool]=None):
     dp = p
     step += 1
     if do_wd and wd != 0:
@@ -236,9 +237,9 @@ def radam_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom
 
     return torch.jit.annotate(Dict[str, Union[Tensor, int]], {'grad_avg': grad_avg, 'sqr_avg': sqr_avg, 'step': step})
 
-# %% ../../nbs/optimizer.torchscript.ipynb 26
+# %% ../../nbs/optimizer.torchscript.ipynb 30
 def RAdam(params, lr, mom=0.9, sqr_mom=0.99, eps=1e-5, wd=0., beta=0., decouple_wd=True, jit=False):
-    "A `Optimizer` for Adam with `lr`, `mom`, `sqr_mom`, `eps` and `params`"
+    "A `Optimizer` or `JitOptimizer` for RAdam with `lr`, `mom`, `sqr_mom`, `eps` and `params`"
     if jit:
         cb = partial(radam_jit_step, decouple_wd=decouple_wd)
         return JitOptimizer(params, cb, lr=lr, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd, beta=beta)
@@ -247,7 +248,7 @@ def RAdam(params, lr, mom=0.9, sqr_mom=0.99, eps=1e-5, wd=0., beta=0., decouple_
         cbs += [partial(average_grad, dampening=True), average_sqr_grad, step_stat, radam_step]
         return Optimizer(params, cbs, lr=lr, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd, beta=beta)
 
-# %% ../../nbs/optimizer.torchscript.ipynb 29
+# %% ../../nbs/optimizer.torchscript.ipynb 34
 @torch.jit.script
 def qhadam_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom:float, eps:float,
                     nu_1:float, nu_2:float, decouple_wd:bool, grad_avg:Optional[Tensor]=None, 
@@ -285,7 +286,7 @@ def qhadam_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mo
 
     return torch.jit.annotate(Dict[str, Union[Tensor, int]], {'grad_avg': grad_avg, 'sqr_avg': sqr_avg, 'step': step})
 
-# %% ../../nbs/optimizer.torchscript.ipynb 30
+# %% ../../nbs/optimizer.torchscript.ipynb 35
 def QHAdam(params, lr, mom=0.999, sqr_mom=0.999, nu_1=0.7, nu_2=1.0, eps=1e-8, wd=0., decouple_wd=True, jit=True):
     "An `Optimizer` for Adam with `lr`, `mom`, `sqr_mom`, `nus`, eps` and `params`"
     if jit:
@@ -296,10 +297,10 @@ def QHAdam(params, lr, mom=0.999, sqr_mom=0.999, nu_1=0.7, nu_2=1.0, eps=1e-8, w
         cbs += [partial(average_grad, dampening=True), average_sqr_grad, step_stat, qhadam_step]
         return Optimizer(params, cbs, lr=lr, nu_1=nu_1, nu_2=nu_2, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd)
 
-# %% ../../nbs/optimizer.torchscript.ipynb 33
+# %% ../../nbs/optimizer.torchscript.ipynb 39
 @torch.jit.script
-def larc_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, eps:float, trust_coeff:float, decouple_wd:bool, 
-                 clip:bool, grad_avg:Optional[Tensor]=None, do_wd:bool=True, dampening:bool=False, force_train:Optional[bool]=None):
+def larc_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, eps:float, trust_coeff:float, decouple_wd:bool,
+                  clip:bool, grad_avg:Optional[Tensor]=None, do_wd:bool=True, dampening:bool=False, force_train:Optional[bool]=None):
     dp = p
     if do_wd and wd != 0:
         if decouple_wd:
@@ -335,7 +336,7 @@ def larc_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, eps:floa
     p.set_(dp)
     return {'grad_avg': grad_avg}
 
-# %% ../../nbs/optimizer.torchscript.ipynb 34
+# %% ../../nbs/optimizer.torchscript.ipynb 40
 def Larc(params, lr, mom=0.9, clip=True, trust_coeff=0.02, eps=1e-8, wd=0., decouple_wd=True, jit=False):
     "A `Optimizer` for Adam with `lr`, `mom`, `sqr_mom`, `eps` and `params`"
     if jit:
@@ -347,7 +348,7 @@ def Larc(params, lr, mom=0.9, clip=True, trust_coeff=0.02, eps=1e-8, wd=0., deco
         cbs += [partial(larc_layer_lr, clip=clip), larc_step]
         return Optimizer(params, cbs, lr=lr, mom=mom, trust_coeff=trust_coeff, eps=eps, wd=wd)
 
-# %% ../../nbs/optimizer.torchscript.ipynb 38
+# %% ../../nbs/optimizer.torchscript.ipynb 45
 @torch.jit.script
 def lamb_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom:float, eps:float, 
                   decouple_wd:bool, grad_avg:Optional[Tensor]=None, sqr_avg:Optional[Tensor]=None, 
@@ -391,7 +392,7 @@ def lamb_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom:
 
     return torch.jit.annotate(Dict[str, Union[Tensor, int]], {'grad_avg': grad_avg, 'sqr_avg': sqr_avg, 'step': step})
 
-# %% ../../nbs/optimizer.torchscript.ipynb 39
+# %% ../../nbs/optimizer.torchscript.ipynb 46
 def Lamb(params, lr, mom=0.9, sqr_mom=0.99, eps=1e-5, wd=0., decouple_wd=True, jit=False):
     "A `Optimizer` for Adam with `lr`, `mom`, `sqr_mom`, `eps` and `params`"
     if jit:
@@ -402,8 +403,100 @@ def Lamb(params, lr, mom=0.9, sqr_mom=0.99, eps=1e-5, wd=0., decouple_wd=True, j
         cbs += [partial(average_grad, dampening=True), average_sqr_grad, step_stat, lamb_step]
         return Optimizer(params, cbs, lr=lr, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd)
 
-# %% ../../nbs/optimizer.torchscript.ipynb 42
-@delegates(RAdam)
-def ranger(p, lr, mom=0.95, wd=0.01, eps=1e-6, jit=False, **kwargs):
+# %% ../../nbs/optimizer.torchscript.ipynb 50
+class JitLookahead(Optimizer):
+    "An `Optimizer` with a modified step for Lookahead TorchScript optimizers"
+    def __init__(self, params:Tensor, cbs:list, train_bn:bool=True, **defaults):
+        super().__init__(params, cbs, train_bn, **defaults)
+        self._init_state()
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        self.count += 1
+        if closure is not None: raise NotImplementedError("fastai optimizers currently do not support closure")
+        for pg, hyper in zip(self.param_lists, self.hypers):
+            for p in pg:
+                if hasattr(p, 'grad') and p.grad is not None:
+                    _update(self.state[p], self.cbs[0](p, p.grad, **{**self.state[p], **hyper}, count=self.count))
+
+    def clear_state(self):
+        super().clear_state()
+        self._init_state()
+
+    def state_dict(self):
+        state = super().state_dict()
+        state.update({'count': self.count})
+        return state
+
+    def load_state_dict(self, sd):
+        self.count = sd.pop('count')
+        super().load_state_dict(sd)
+
+    def _init_state(self): 
+        self.count = 0
+
+# %% ../../nbs/optimizer.torchscript.ipynb 53
+@torch.jit.script
+def ranger_jit_step(p:Tensor, grad:Tensor, lr:float, wd:float, mom:float, sqr_mom:float, eps:float, beta:float,
+                    decouple_wd:bool, count:int, k:int, alpha:float, grad_avg:Optional[Tensor]=None, sqr_avg:Optional[Tensor]=None,
+                    slow_p:Optional[Tensor]=None, do_wd:bool=True, step:int=0, force_train:Optional[bool]=None):
+    dp = p
+    step += 1
+    if slow_p is None: 
+        slow_p = dp.clone().detach()
+
+    if do_wd and wd != 0:
+        if decouple_wd:
+            # weight_decay
+            dp = dp.mul(1 - lr*wd)
+        else:
+            # l2_reg
+            grad = grad.add(dp, alpha=wd)
+
+    if grad_avg is None: 
+        grad_avg = torch.zeros_like(dp, memory_format=torch.preserve_format)
+    if sqr_avg is None: 
+        sqr_avg  = torch.zeros_like(dp, memory_format=torch.preserve_format)
+
+    # average_grad, dampening=True
+    grad_avg = torch.mul(grad_avg, mom)
+    grad_avg = torch.add(grad_avg, grad, alpha=1-mom)
+
+    # average_sqr_grad
+    sqr_avg = torch.mul(sqr_avg, sqr_mom)
+    sqr_avg = torch.addcmul(sqr_avg, grad, grad, value=1-sqr_mom)
+
+    # radam_step
+    debias1 = debias(mom, step)
+    debias2 = debias(sqr_mom, step)
+    r_inf = 2/(1-sqr_mom) - 1
+    r = r_inf - 2*step*sqr_mom**step/(1-sqr_mom**step)
+    
+    if r > 5:
+        v = math.sqrt(((r-4) * (r-2) * r_inf)/((r_inf-4)*(r_inf-2)*r))
+        denom = torch.sqrt(sqr_avg/debias2)
+        if eps != 0: 
+            denom = denom + eps
+        if beta != 0: 
+            denom = F.softplus(denom, beta)
+        dp = torch.addcdiv(dp, grad_avg, denom, value = -lr*v / debias1)
+    else:
+        dp = torch.add(dp, grad_avg, alpha=-lr / debias1)
+
+    # lookahead step
+    if count % k != 0:
+        p.set_(dp)
+    else:
+        slow_p = torch.add(slow_p, torch.sub(dp, slow_p), alpha=alpha)
+        p.set_(slow_p)
+    
+    return torch.jit.annotate(Dict[str, Union[Tensor, int]], {'grad_avg': grad_avg, 'sqr_avg': sqr_avg, 'step': step, 'slow_p': slow_p})
+
+# %% ../../nbs/optimizer.torchscript.ipynb 54
+def ranger(params, lr, mom=0.95, sqr_mom=0.99, eps=1e-6, wd=0.01, beta=0., k=6, alpha=0.5, decouple_wd=True, jit=False):
     "Convenience method for `Lookahead` with `RAdam`"
-    return Lookahead(RAdam(p, lr=lr, mom=mom, wd=wd, eps=eps, jit=jit, **kwargs))
+    if jit:
+        cb = partial(ranger_jit_step, decouple_wd=decouple_wd)
+        return JitLookahead(params, cb, lr=lr, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd, beta=beta, k=k, alpha=alpha)
+    else:
+        return Lookahead(RAdam(params, lr=lr, mom=mom, sqr_mom=sqr_mom, eps=eps, wd=wd, beta=beta), k=k, alpha=alpha)
