aa2e64b0086cbfdb89b42d11a049ff7c413c1dce
Benjamin Warner <me@benjaminwarner.dev>
2022-10-28 15:51:41 -0500

    XResNet compatibility with vision_learner & itemtensor notebook name

--- fastxtend/vision/models/xresnet.py
+++ fastxtend/vision/models/xresnet.py
@@ -126,7 +126,7 @@ class XResNet(nn.Sequential):
     def __init__(self, block, expansion, layers, p=0.0, c_in=3, n_out=1000, stem_szs=(32,32,64), 
                  block_szs=[64,128,256,512], widen=1.0, sa=False, act_cls=defaults.activation, ndim=2, 
                  ks=3, stride=2, stem_layer=ConvLayer, stem_pool=MaxPool, head_pool=AdaptiveAvgPool, 
-                 custom_head=None, **kwargs):
+                 custom_head=None, pretrained=False, **kwargs):
         store_attr('block,expansion,act_cls,ndim,ks')
         if ks % 2 == 0: raise Exception('kernel size has to be odd!')
         stem_szs = [c_in, *stem_szs]
