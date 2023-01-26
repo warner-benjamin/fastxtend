b8e84c9c83ca7b81154e2dbddd436173d0b72198
Benjamin Warner <me@benjaminwarner.dev>
2023-01-25 18:45:53 -0600

    Add LastMetricCallback

--- fastxtend/callback/tracker.py
+++ fastxtend/callback/tracker.py
@@ -13,7 +13,7 @@ from fastai.callback.tracker import SaveModelCallback, TrackerCallback
 from ..imports import *
 
 # %% auto 0
-__all__ = ['TerminateOnTrainNaN', 'SaveModelAtEnd']
+__all__ = ['TerminateOnTrainNaN', 'SaveModelAtEnd', 'LastMetricCallback']
 
 # %% ../../nbs/callback.tracker.ipynb 6
 class TerminateOnTrainNaN(Callback):
@@ -43,3 +43,30 @@ class SaveModelAtEnd(SaveModelCallback):
 
     def after_fit(self, **kwargs):
         self.last_saved_path = self.learn.save(f'{self.fname}', with_opt=self.with_opt)
+
+# %% ../../nbs/callback.tracker.ipynb 11
+class LastMetricCallback(Callback):
+    "A `Callback` which stores metrics by name in a `Learner.lastmetric` dictionary"
+    order,remove_on_fetch,_only_train_loop = 60,True,True
+    def __init__(self, metrics:Listified[str]='valid_loss'):
+        self.metrics=L(metrics)
+
+    def before_fit(self):
+        "Prepare the monitored value(s)"
+        self.run = not hasattr(self, "lr_finder") and not hasattr(self, "gather_preds")
+        self.idx, self.learn.lastmetric = [], {}
+        for m in self.metrics:
+            assert m in self.recorder.metric_names[1:], f'Metric {m} does not exist'
+            self.idx.append(list(self.recorder.metric_names[1:]).index(m))
+
+    def after_fit(self):
+        "Store the last the monitored value(s)"
+        for i, idx in enumerate(self.idx):
+            self.learn.lastmetric[self.metrics[i]] = self.recorder.values[-1][idx]
+        self.run = True
+
+    def after_fit_exception(self):
+        try:
+            self.after_fit()
+        finally:
+            self.run = True
