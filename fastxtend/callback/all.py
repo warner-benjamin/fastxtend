4328f7081ab99f05604d7099628c441de5cc5af4
Benjamin Warner <me@benjaminwarner.dev>
2023-02-15 01:18:44 -0600

    Harmonize with fastai 2.7.11

--- fastxtend/callback/all.py
+++ fastxtend/callback/all.py
@@ -1,5 +1,11 @@
-from . import channelslast, lr_finder
+from packaging.version import parse
+import fastai
+
+from .lr_finder import *
 from .cutmixup import *
 from .ema import *
 from .progresize import *
 from .tracker import *
+
+if parse(fastai.__version__) < parse('2.7.11'):
+    from .channelslast import *
