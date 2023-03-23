cc631bc868b3e3cdd5089b5808e1945da2f77ac7
Benjamin Warner <me@benjaminwarner.dev>
2023-03-23 15:23:05 -0500

    Import fastxtend DatasetWriter from all

--- fastxtend/ffcv/all.py
+++ fastxtend/ffcv/all.py
@@ -1,7 +1,6 @@
 from ffcv.fields import Field, BytesField, IntField, FloatField, NDArrayField, JSONField, TorchTensorField
 from ffcv.fields.decoders import *
 from ffcv.pipeline import *
-from ffcv.writer import DatasetWriter
 
 import fastxtend.ffcv.ft as ft
 
@@ -9,3 +8,4 @@ from .fields import RGBImageField
 from .loader import *
 from .inference import *
 from .utils import *
+from .writer import *
