c4ecb627c56902811dab1f885f10c9e0440f5103
Benjamin Warner <me@benjaminwarner.dev>
2023-08-12 18:30:53 -0500

    Minimal Hugging Face Transformers compatibility

---
+++ fastxtend/text/all.py
@@ -0,0 +1,19 @@
+from packaging.version import parse
+import fastai
+
+from ..basics import *
+if parse(fastai.__version__) < parse('2.7.13'):
+    from ..callback.amp import *
+    from ..callback.channelslast import *
+from ..callback.ema import *
+from ..callback.gradaccum import *
+from ..callback.lr_finder import *
+from ..callback.tracker import *
+
+from ..data.all import *
+from ..losses import *
+from ..metrics import *
+from ..multiloss import *
+from ..optimizer.all import *
+from ..schedulers import *
+from ..utils import *
