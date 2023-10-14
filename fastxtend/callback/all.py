bf2d2efafcf28c92250a0475835b841e76f79bc7
Benjamin Warner <me@benjaminwarner.dev>
2023-10-14 17:42:21 -0500

    Mark GradAccum as beta feature

--- fastxtend/callback/all.py
+++ fastxtend/callback/all.py
@@ -4,9 +4,9 @@ import fastai
 if parse(fastai.__version__) < parse('2.7.13'):
     from .amp import *
     from .channelslast import *
+
 from .cutmixup import *
 from .ema import *
-from .gradaccum import *
 from .lr_finder import *
 from .progresize import *
 from .tracker import *
--- fastxtend/text/all.py
+++ fastxtend/text/all.py
@@ -5,8 +5,8 @@ from ..basics import *
 if parse(fastai.__version__) < parse('2.7.13'):
     from ..callback.amp import *
     from ..callback.channelslast import *
+
 from ..callback.ema import *
-from ..callback.gradaccum import *
 from ..callback.lr_finder import *
 from ..callback.tracker import *
 
