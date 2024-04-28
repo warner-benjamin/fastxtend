1c8dd532e2e2826d73577131b33e898a3b359f56
Benjamin Warner <me@benjaminwarner.dev>
2024-04-27 19:07:47 -0500

    update opencv version

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 conda create -n fastxtend python=3.11 "pytorch>=2.1" torchvision torchaudio \
-pytorch-cuda=12.1 fastai nbdev pkg-config libjpeg-turbo opencv tqdm psutil \
+pytorch-cuda=12.1 fastai nbdev pkg-config libjpeg-turbo "opencv<4.8" tqdm psutil \
 terminaltables numpy "numba>=0.57" "librosa>=0.10.1" timm kornia rich typer wandb \
 "transformers>=4.34" "tokenizers>=0.14" "datasets>=2.14" ipykernel ipywidgets \
-"matplotlib<3.8" -c pytorch -c nvidia -c fastai -c huggingface -c conda-forge
+"matplotlib<3.8" quarto -c pytorch -c nvidia -c fastai -c huggingface -c conda-forge
